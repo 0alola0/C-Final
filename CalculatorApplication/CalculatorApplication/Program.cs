fcc380f8c0ee4f3e536ef0be7209985ead222c21
Vato Loladze <a.loladze16@gmail.com>
2022-12-21 16:43:13 +0400

    added a start method with multiple options

--- CalculatorApplication/CalculatorApplication/Program.cs
+++ CalculatorApplication/CalculatorApplication/Program.cs
@@ -20,14 +20,57 @@ namespace CalculatorApplication
 
             string instructionAnswer = Console.ReadLine();
 
-            if (instructionAnswer == "Y")
+            if (instructionAnswer == "Y"|| instructionAnswer == "y")
             {
                 Console.WriteLine(
                     "Welcome to Calendar, please read the instructions carefully to ensure a productive use:"
                     );
+                
             }
 
+            Start();
+
             
+        }
+
+        public static void Start()
+        {
+            Console.WriteLine("Let us start!");
+            Console.WriteLine("what would you like to do? choose your oprion by typing corresponding letter");
+            Console.WriteLine("A. add a new event");
+            Console.WriteLine("B. find an event and amend it");
+            Console.WriteLine("C. see all events");
+            Console.WriteLine("D. delete an event");
+
+            string actionAnswer = Console.ReadLine();
+
+            if (actionAnswer == "A" || actionAnswer == "a")
+            {
+
+            }
+
+            else if (actionAnswer == "B" || actionAnswer == "b")
+            {
+
+            }
+
+            else if (actionAnswer == "C" || actionAnswer == "c")
+            {
+
+            }
+
+            else if (actionAnswer == "D" || actionAnswer == "d")
+            {
+
+            }
+
+            else
+            {
+                Console.WriteLine("please choose from available answers");
+                Start();
+            }
+
+
         }
 
 
